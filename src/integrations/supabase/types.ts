d13f7b3371d47f4526200c6afceab87dd3badada
warrenleroux <warren@busstech.com>
2025-11-17 08:30:57 +0200

    chore: Regenerate TypeScript types from production database

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -7,6 +7,11 @@ export type Json =
   | Json[]
 
 export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "13.0.5"
+  }
   graphql_public: {
     Tables: {
       [_ in never]: never
@@ -2570,4 +2575,3 @@ export const Constants = {
     },
   },
 } as const
-
